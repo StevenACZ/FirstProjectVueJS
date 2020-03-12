cfd9d738bc88cab1f598dffcbf4e47e7fd6b6654
Steven Coaila Zaa <scoailazaa@gmail.com>
2020-03-12 17:06:34 -0500

    Simple project

--- app.js
+++ app.js
@@ -4,7 +4,32 @@ new Vue({
   data() {
     return {
       name: 'Bitcoin',
-      img: 'https://cryptologos.cc/logos/bitcoin-btc-logo.png'
+      img: 'https://cryptologos.cc/logos/bitcoin-btc-logo.png',
+      changePercent: 10,
+
+      color: 'f4f4f4',
+
+      price: 8400,
+
+      pricesWithDays: [
+        { day: 'Lunes', value: 8400 },
+        { day: 'Martes', value: 7900 },
+        { day: 'Miercoles', value: 8200 },
+        { day: 'Jueves', value: 9000 },
+        { day: 'Viernes', value: 9400 },
+        { day: 'Sabado', value: 10000 },
+        { day: 'Domingo', value: 10200 },
+      ],
+
+      showPrices: false
+    }
+  },
+  
+  methods: {
+    toggleShowPrices() {
+      this.showPrices = !this.showPrices;
+
+      this.color = this.color.split('').reverse().join('');
     }
   }
 })
